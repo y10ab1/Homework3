04ec38f51613c73fa10f0d66c17d9d4109c11a6e
y10ab1 <brian880825@gmail.com>
2024-05-12 12:32:56 +0800

    p4

--- Markowitz_2.py
+++ Markowitz_2.py
@@ -55,32 +55,100 @@ class MyPortfolio:
     NOTE: You can modify the initialization function
     """
 
-    def __init__(self, price, exclude, lookback=50, gamma=0):
+    def __init__(self, price, exclude, lookback=300, gamma=0):
         self.price = price
         self.returns = price.pct_change().fillna(0)
         self.exclude = exclude
         self.lookback = lookback
         self.gamma = gamma
 
+    # def calculate_weights(self):
+    #     # Get the assets by excluding the specified column
+    #     assets = self.price.columns[self.price.columns != self.exclude]
+
+    #     # Calculate the portfolio weights
+    #     self.portfolio_weights = pd.DataFrame(
+    #         index=self.price.index, columns=self.price.columns
+    #     )
+
+    #     """
+    #     TODO: Complete Task 4 Below
+    #     """
+        
+
+    #     """
+    #     TODO: Complete Task 4 Above
+    #     """
+
+    #     self.portfolio_weights.ffill(inplace=True)
+    #     self.portfolio_weights.fillna(0, inplace=True)
+    
     def calculate_weights(self):
         # Get the assets by excluding the specified column
         assets = self.price.columns[self.price.columns != self.exclude]
 
         # Calculate the portfolio weights
-        self.portfolio_weights = pd.DataFrame(
-            index=self.price.index, columns=self.price.columns
-        )
+        self.portfolio_weights = pd.DataFrame(index=df.index, columns=df.columns)
 
-        """
-        TODO: Complete Task 4 Below
-        """
-
-        """
-        TODO: Complete Task 4 Above
-        """
+        for i in range(self.lookback + 1, len(df)):
+            R_n = self.returns.copy()[assets].iloc[i - self.lookback : i]
+            self.portfolio_weights.loc[df.index[i], assets] = self.mv_opt(
+                R_n, self.gamma
+            )
 
         self.portfolio_weights.ffill(inplace=True)
         self.portfolio_weights.fillna(0, inplace=True)
+
+    def mv_opt(self, R_n, gamma):
+        Sigma = R_n.cov().values
+        mu = R_n.mean().values
+        n = len(R_n.columns)
+
+        with gp.Env(empty=True) as env:
+            env.setParam("OutputFlag", 0)
+            env.setParam("DualReductions", 0)
+            env.start()
+            with gp.Model(env=env, name="portfolio") as model:
+                """
+                TODO: Complete Task 3 Below
+                """
+
+                # Sample Code: Initialize Decision w and the Objective
+                # NOTE: You can modify the following code
+                w = model.addMVar(n, name="w", ub=1)
+                # model.setObjective(w.sum(), gp.GRB.MAXIMIZE)
+                
+                model.setObjective(w @ mu - (gamma/2) * w @ Sigma @ w, gp.GRB.MAXIMIZE)
+
+                model.addConstr(w.sum() == 1)
+                model.addConstr(w >= 0)
+
+                """
+                TODO: Complete Task 3 Below
+                """
+                model.optimize()
+
+                # Check if the status is INF_OR_UNBD (code 4)
+                if model.status == gp.GRB.INF_OR_UNBD:
+                    print(
+                        "Model status is INF_OR_UNBD. Reoptimizing with DualReductions set to 0."
+                    )
+                elif model.status == gp.GRB.INFEASIBLE:
+                    # Handle infeasible model
+                    print("Model is infeasible.")
+                elif model.status == gp.GRB.INF_OR_UNBD:
+                    # Handle infeasible or unbounded model
+                    print("Model is infeasible or unbounded.")
+
+                if model.status == gp.GRB.OPTIMAL or model.status == gp.GRB.SUBOPTIMAL:
+                    # Extract the solution
+                    solution = []
+                    for i in range(n):
+                        var = model.getVarByName(f"w[{i}]")
+                        # print(f"w {i} = {var.X}")
+                        solution.append(var.X)
+
+        return solution
 
     def calculate_portfolio_returns(self):
         # Ensure weights are calculated
